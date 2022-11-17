26ff09082f354dd30047e7161cb5905425df3e0f
patzgn <103074854+patzgn@users.noreply.github.com>
2022-11-17 22:49:47 +0100

    first commit

---
+++ src/main/java/com/patzgn/geekcollection/GeekCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.patzgn.geekcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patzgn/geekcollection/GeekCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patzgn.geekcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
